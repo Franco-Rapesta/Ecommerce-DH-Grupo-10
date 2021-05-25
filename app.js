d289a1cabbe3ac483868b6b43c8f594c7ee7f0dc
luccaOpro <luccaopro@gmail.com>
2021-05-25 20:45:26 -0300

    base

--- app.js
+++ app.js
@@ -16,24 +16,3 @@ app.use(express.static(path.resolve(__dirname, '/public')));
 app.listen(4000, () => {
     console.log("Servidor corriendo");
 });
-
-/* const express = require('express');
-const path = require('path');
-const app = express();
-
-app.get('/public', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, '/views/index.html'));
-    res.sendFile(path.resolve(__dirname, '/views/login.html')); 
-    res.sendFile(path.resolve(__dirname, '/views/registro.html')); 
-    res.sendFile(path.resolve(__dirname, '/views/detalle-producto.html')); 
-    res.sendFile(path.resolve(__dirname, '/views/carrito.html')); 
-});
-
-
-app.use(express.static(path.resolve(__dirname, '/public')));
-
-app.listen(4000, () => {
-    console.log("Servidor corriendo");
-});
-
- */
