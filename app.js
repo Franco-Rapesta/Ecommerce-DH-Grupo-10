26e308ba47df3a06cd0421e479959d4f866c68ba
Francorapesta@gmail.com <Francorapesta@gmail.com>
2021-05-28 13:02:45 -0300

    CAMBIOS APP.JS

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/public', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, '/views/index.html'));
-    res.sendFile(path.resolve(__dirname, '/views/login.html')); 
-    res.sendFile(path.resolve(__dirname, '/views/registro.html')); 
-    res.sendFile(path.resolve(__dirname, '/views/detalle-producto.html')); 
-    res.sendFile(path.resolve(__dirname, '/views/carrito.html')); 
-});
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
 
-app.use(express.static(path.resolve(__dirname, '/public')));
-
-app.listen(4000, () => {
+app.listen(5500, () => {
     console.log("Servidor corriendo");
+});     
+
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+    res.sendFile(path.resolve(__dirname, './views/login.html')); 
+    res.sendFile(path.resolve(__dirname, './views/registro.html')); 
+    res.sendFile(path.resolve(__dirname, './views/detalle-producto.html')); 
+    res.sendFile(path.resolve(__dirname, './views/carrito.html')); 
 });
+
+
+
+
