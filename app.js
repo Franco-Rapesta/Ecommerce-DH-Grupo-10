1a0f81779dda1da14d23c004624b369f4ea2cb98
IndustrialBot <juano94@gmail.com>
2021-05-27 22:30:36 -0300

    Va update Juan al 27 Mayo

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, '/public')));
 
 app.get('/public', (req,res) =>{
     res.sendFile(path.resolve(__dirname, '/views/index.html'));
@@ -10,8 +12,6 @@ app.get('/public', (req,res) =>{
     res.sendFile(path.resolve(__dirname, '/views/carrito.html')); 
 });
 
-
-app.use(express.static(path.resolve(__dirname, '/public')));
 
 app.listen(4000, () => {
     console.log("Servidor corriendo");
