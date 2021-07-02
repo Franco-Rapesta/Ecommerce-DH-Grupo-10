6251495179e2a798ba38f00dbbb4c6e820c4157e
IndustrialBot <juano94@gmail.com>
2021-07-01 21:48:11 -0300

    Commite Juan Database

--- app.js
+++ app.js
@@ -29,30 +29,6 @@ app.listen(3002, () => {
 
 
 
-//  app.get("/", function(req,res){
-//     res.sendFile(__dirname + "/views/index.html")
 
-// });
-
-// app.get("/login", function(req,res){
-//     res.sendFile(__dirname + "/views/login.html")
-
-// });
-
-
-// app.get("/registro", function(req,res){
-//     res.sendFile(__dirname + "/views/registro.html")
-
-// });
-
-// app.get("/detalle-producto", function(req,res){
-//     res.sendFile(__dirname + "/views/detalle-producto.html")
-
-// });
-// app.get("/carrito", function(req,res){
-//     res.sendFile(__dirname + "/views/carrito.html")
-
-//});
-    
 
 
